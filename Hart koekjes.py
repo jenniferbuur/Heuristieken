ded93bfe586aa4457ba99270356d52c2f5c51e52
SemStu <sem@MacBook-Pro-van-Sem.local>
2017-04-11 21:27:48 +0200

    Import data in dictionary

--- Hart koekjes.py
+++ Hart koekjes.py
@@ -1,1 +1,10 @@
-print("Hart koekjes")
+
+dictionary = {}
+
+## insert data into dictionary
+with open('/Users/sem/Documents/Heuristieken/CargoLists/CargoList1.txt', 'rU') as f:
+    for line in f:
+            split = line.split()
+            dictionary[split[0]] = split[1:]
+
+print(dictionary)
