eeb3b152a914f4951ffb0d2108066260251ad26f
emmahokken <emmahokken@wcw-stud-145-109-55-160.wireless.uva.nl>
2017-05-31 11:27:51 +0200

    Main met comments

--- main.py
+++ main.py
@@ -7,30 +7,9 @@ import random
 # alle lists inladen
 # importeer alle classes
 
-<<<<<<< Updated upstream
-=======
 # alle cargolists in een dictionary
-cargolist1 = {}; cargolist2 = {}; cargolist3 = {}
-lists = [cargolist1, cargolist2, cargolist3]
-nritems = [0, 0, 0]
 weight = 0; space = 1; ratio = 2;
 
-# open the cargolists and replace the comma with a period and puts it in dictionaries for each cargolist
-# deze ook naar helpers?
-for i in range(1,4):
-    with open('CargoLists/CargoList' + str(i) + '.txt', 'rU') as f:
-        for line in f:
-            split = line.split()
-            key = split[0][4:]
-            if len(split) == 3:
-                split[2] = split[2].replace(",", ".")
-                lists[i-1][key] = [float(split[1]), float(split[2])]
-                lists[i-1][key].append(lists[i-1][key][weight]/lists[i-1][key][space])
-                nritems[i-1] += 1
-
-items1 = nritems[0]; items2 = nritems[1]; items3 = nritems[2]
-
->>>>>>> Stashed changes
 # initialise spacecrafts in class
 cygnus = sc.Spacecrafts("Cygnus", 2000, 18.9)
 verne = sc.Spacecrafts("Verne", 2300, 13.1)
@@ -47,9 +26,12 @@ spacecrafts = [cygnus, verne, progress, kounotori]
 cargolist = helpers.importlist(1)
 items = len(cargolist.keys())
 best_weight = 10000
+
+# iterate over permutations
 for crafts in itertools.permutations(spacecrafts):
     helpers.weight(cargolist, crafts)
     solution = helpers.solution(crafts, items)
+    # remember best solution
     if solution[0] < best_weight:
         best_weight = solution[0]
         best_space = solution[1]
@@ -68,18 +50,15 @@ print('Wasted weight: ' + str(solution[0]) + ', Wasted space: ' + str(solution[1
 # partly brute force on shortened cargolist
 optimized = helpers.optimize(cargolist, spacecrafts)
 bestscore = 10000
-<<<<<<< Updated upstream
-for bruteforce in range(0, 1):
-=======
-# eventueel functie van maken in algoritmes zodat je geen for loops meer hebt in main
-# want is eigenlijk een algoritme
+
 for bruteforce in range(0, 100000):
->>>>>>> Stashed changes
     randomlist = optimized.keys()
     random.shuffle(randomlist)
     helpers.ratio(optimized, randomlist, spacecrafts)
     solution = helpers.solution(spacecrafts, items)
+    # score function
     score = solution[0] + 0.01 * solution[2]
+    # remember best solution
     if score < bestscore:
         bestscore = score
         print('#: ' + str(bruteforce) + ', Score: ' + str(score))
@@ -90,9 +69,11 @@ for bruteforce in range(0, 100000):
 cargolist = helpers.importlist(2)
 items = len(cargolist.keys())
 best_weight = 10000
+# iterate over permutations
 for crafts in itertools.permutations(spacecrafts):
     helpers.weight(cargolist, crafts)
     solution = helpers.solution(crafts, items)
+    # remember best solution
     if solution[0] < best_weight:
         best_weight = solution[0]
         best_space = solution[1]
@@ -110,12 +91,14 @@ print('Wasted weight: ' + str(solution[0]) + ', Wasted space: ' + str(solution[1
 # partly brute force on shortened cargolist
 optimized = helpers.optimize(cargolist, spacecrafts)
 bestscore = 10000
-for bruteforce in range(0, 1):
+for bruteforce in range(0, 10000):
     randomlist = optimized.keys()
     random.shuffle(randomlist)
     helpers.ratio(optimized, randomlist, spacecrafts)
     solution = helpers.solution(spacecrafts, items)
+    # score function
     score = solution[0] + 0.01 * solution[2]
+    # remember best solution
     if score < bestscore:
         bestscore = score
         print('#: ' + str(bruteforce) + ', Score: ' + str(score))
@@ -125,13 +108,17 @@ for bruteforce in range(0, 1):
 # loading a massive cargolist (3) into multiple spacecrafts
 # with this every country has to send an equal amount with a maximum difference of 1
 cygnus = []; verne = []; progress = []; kounotori = []; tianzhou = []; dragon = []
-packed_cargo = []; speed = 1000; last_list = False; number_of_sc = 0
+packed_cargo = []; speed = 1000; number_of_sc = 0
+
+# load cargolist
 cargolist = helpers.importlist(3); items = len(cargolist.keys())
+
 
 for cycle in range(0, 100):
     print('Cycle: ' + str(cycle))
     bestscore = 99999999
 
+    # make new spacecrafts for every new cycle
     cygnus.append(sc.Spacecrafts("Cygnus", 2000, 18.9))
     verne.append(sc.Spacecrafts("Verne", 2300, 13.1))
     progress.append(sc.Spacecrafts("Progress", 2400, 7.6))
@@ -139,31 +126,38 @@ for cycle in range(0, 100):
     tianzhou.append(sc.Spacecrafts("TianZhou", 6500, 15))
     dragon.append(sc.Spacecrafts("Dragon", 3400, 42))
 
+    # used spacecrafts for every cycle excluding the last cycle
     spacecrafts = [verne[cycle], progress[cycle], kounotori[cycle], tianzhou[cycle], dragon[cycle]]
+
+    # pull all items packed before
     for item in packed_cargo:
         cargolist.pop(item)
 
     print('Cargo left: ' + str(len(cargolist)))
+
+    # if there are no cargoitems left, break
     if len(cargolist) == 0:
         print('The total number of spacecrafts is: ' + str(number_of_sc))
         print('')
         break
 
+    # shorten list and randomize it
     shortened = helpers.shorten(cargolist, spacecrafts)
     randomlist = shortened.keys()
     all_spacecrafts = [cygnus[cycle], verne[cycle], progress[cycle], kounotori[cycle], tianzhou[cycle], dragon[cycle]]
 
-    if len(optimized) == len(cargolist):
-        last_list = True; last_weight = 0; last_space = 0;
+    # if the last cycle has been reached, change spacecrafts
+    if len(shortened) == len(cargolist):
+        last_weight = 0; last_space = 0;
         for item in optimized:
             last_weight += optimized[item][weight]
             last_space += optimized[item][space]
-
-    if last_list == True:
         spacecrafts = helpers.spacecrafts(last_weight, last_space, all_spacecrafts)
 
+    # start bruteforce
     for bruteforce in range(0, speed):
 
+        # set spacecrafts to original
         if len(spacecrafts) == 1:
             spacecrafts.set_orignal()
         else:
@@ -175,11 +169,10 @@ for cycle in range(0, 100):
         helpers.ratio(shortened, randomlist, spacecrafts)
         score = helpers.score(spacecrafts, items)
 
+        # remember best score
         if score[0] < bestscore:
             bestscore = score[0]
             packed_cargo = []
-            # print('#: ' + str(bruteforce) + ', Score: ' + str(score[0]))
-            # print('Wasted weight: ' + str(score[1]) + ', Wasted space: ' + str(score[2]) + '.')
             for craft in spacecrafts:
                 packed_cargo.extend(craft.cargolist)
 
@@ -191,13 +184,14 @@ for cycle in range(0, 100):
 # the last restriction doesn't matter any more
 
 cygnus = []; verne = []; progress = []; kounotori = []; tianzhou = []; dragon = []
-packed_cargo = []; speed = 1000; last_list = False; number_of_sc = 0
+packed_cargo = []; speed = 1000; number_of_sc = 0
 cargolist = helpers.importlist(3); items = len(cargolist.keys())
 
 for cycle in range(0, 100):
     print('Cycle: ' + str(cycle))
     bestscore = 99999999
 
+    # make new spacecrafts for every new cycle
     cygnus.append(sc.Spacecrafts("Cygnus", 2000, 18.9))
     verne.append(sc.Spacecrafts("Verne", 2300, 13.1))
     progress.append(sc.Spacecrafts("Progress", 2400, 7.6))
@@ -205,7 +199,7 @@ for cycle in range(0, 100):
     tianzhou.append(sc.Spacecrafts("TianZhou", 6500, 15))
     dragon.append(sc.Spacecrafts("Dragon", 3400, 42))
 
-    # aanpassen op hoe je wil indelen, met resticties e.d.
+    # take biggest spacecrafts to fill
     spacecrafts = [tianzhou[cycle], dragon[cycle]]
     for item in packed_cargo:
         cargolist.pop(item)
@@ -219,15 +213,15 @@ for cycle in range(0, 100):
     randomlist = shortened.keys()
     all_spacecrafts = [cygnus[cycle], verne[cycle], progress[cycle], kounotori[cycle], tianzhou[cycle], dragon[cycle]]
 
+    # if last cycle has been reached
     if len(optimized) == len(cargolist):
-        last_list = True; last_weight = 0; last_space = 0;
+        last_weight = 0; last_space = 0;
         for item in optimized:
             last_weight += optimized[item][weight]
             last_space += optimized[item][space]
-
-    if last_list == True:
         spacecrafts = helpers.spacecrafts(last_weight, last_space, all_spacecrafts)
 
+    # start bruteforce
     for bruteforce in range(0, speed):
 
         if len(spacecrafts) == 1:
@@ -241,6 +235,7 @@ for cycle in range(0, 100):
         helpers.ratio(shortened, randomlist, spacecrafts)
         score = helpers.score(spacecrafts, items)
 
+        # remember best score
         if score[0] < bestscore:
             bestscore = score[0]
             packed_cargo = []
